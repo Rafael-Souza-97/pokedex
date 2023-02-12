eee01654a1cb26652507e9aef962090f9dad6adf
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-12 16:24:35 -0300

    feat Add SearchBar Component

---
+++ Pokedex/src/components/SearchBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Pesquise por um Pokémon'
+        className='search-input'
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
--- Pokedex/src/pages/Home.tsx
+++ Pokedex/src/pages/Home.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Cards from '../components/Cards';
 import Header from '../components/Header';
 import Pagination from '../components/Pagination';
+import SearchBar from '../components/SearchBar';
+import '../styles/Home.css';
 
 function Home() {
   return (
     <>
       <Header />
-      <div>
+      <div className='search-pagination'>
+        <SearchBar />
         <Pagination />
       </div>
       <Cards />
