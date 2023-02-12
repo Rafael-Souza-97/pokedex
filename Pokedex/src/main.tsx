e5ef1adf8e9997ca08e9165869ca0189214c2bb4
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-12 13:10:14 -0300

    feat: Add About Page

--- Pokedex/src/main.tsx
+++ Pokedex/src/main.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { PokemonsContextProvider } from '../src/context/pokemonProvider';
-import Comparison from './pages/Comparison';
-import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import PokemonDetail from './pages/PokemonDetail';
+import Comparison from './pages/Comparison';
+import Favorites from './pages/Favorites';
+import About from './pages/About';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <PokemonsContextProvider>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path='/pokemon/:id' component={PokemonDetail} /> 
         <Route exact path="/favorites" component={Favorites} />
         <Route exact path="/comparison" component={Comparison} />
-        <Route path='/pokemon/:id' component={PokemonDetail} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </PokemonsContextProvider>
   </BrowserRouter>
