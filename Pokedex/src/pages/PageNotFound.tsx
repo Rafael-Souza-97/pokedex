39f9774fcb7640d3e4615578a21bb199a6b58547
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-13 03:24:42 -0300

    feat: Page Not Found config

--- Pokedex/src/pages/PageNotFound.tsx
+++ Pokedex/src/pages/PageNotFound.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import '../styles/PageNotFound.css';
+import { Link } from 'react-router-dom';
+import sadPokemon from '../assets/sad-pokemon.jpg';
+import error404 from '../assets/404.png';
+import '../styles/NotFound.css';
 
-function PageNotFound() {
+const NotFound: React.FC = () => {
   return (
-    <div>Page Not Found</div>
+    <div className="not-found-container">
+      <img
+        className="error404-image"
+        src={ error404 }
+        alt="Pikachu crying because the page requested was not found"
+      />
+      <h1 className="not-found-request-text">
+        Page requested not found
+        <span role="img" aria-label="Crying emoji"> 😭</span>
+      </h1>
+      <p className="page-not-found-text">
+        Desculpe, a página que você está tentando acessar não foi encontrada.
+      </p>
+      <img
+        className="not-found-image"
+        src={ sadPokemon }
+        alt="Pikachu crying because the page requested was not found"
+      />
+      <Link to="/">
+        <button type="button" className="redirect-button">Voltar para a página inicial</button>
+      </Link>
+    </div>
   );
-}
+};
 
-export default PageNotFound;
+export default NotFound;
