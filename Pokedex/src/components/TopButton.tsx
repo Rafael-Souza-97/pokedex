26d95d9cfc829f6e2ff2b9bb6527f2c3b5d3d86d
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-15 03:54:57 -0300

    test: Add Button to get window top

---
+++ Pokedex/src/components/TopButton.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { RxDoubleArrowUp } from 'react-icons/rx';
+import '../styles/TopButton.css';
+
+const TopButton: React.FC = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handleScroll() {
+    const scrollPos = window.scrollY;
+
+    console.log(scrollPos);
+
+    if (scrollPos > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }
+
+  function handleClick() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  return (
+    isVisible && (
+      <button
+        onClick={handleClick}
+        className="to-top-button"
+      >
+        <RxDoubleArrowUp size={25} color={'white'} />
+      </button>
+    )
+  ) || null;
+};
+
+export default TopButton;
