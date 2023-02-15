23bd8f26f2bff10f1b1257a9374d9be26ba81068
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-15 18:43:48 -0300

    style: Pokemon Details Responsiveness

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
 import FavoritesCards from '../components/FavoritesCards';
-import '../styles/Favorites.css';
 
 function Favorites() {
   return (
