92a5da8812e3b53fffa2423b0f24cbee0c3853e9
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-18 23:10:32 -0300

    style: Header styles on Dark-Mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { PokemonsContext } from '../context';
 import DarkMode from './DarkMode';
 import pokemonLogo from '../assets/pokemon-logo.png';
+import '../styles/Dark-Mode/Header-Dark.css';
 import '../styles/Header.css';
 
 const Header: React.FC = () => {
+  const { isDarkModeOn } = useContext(PokemonsContext);
+
+  useEffect(() => { 
+    if (isDarkModeOn) { 
+      document.body.classList.add('dark-mode');
+    } else { 
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkModeOn]);
+
   const handleLogoClick = () => {
     window.location.href = '/';
   };
 
   return (
-    <header className="header" data-testid="header">
+    <header className={ isDarkModeOn ? 'header dark-mode' : 'header' } data-testid='header' >
       <DarkMode />
 
-      <div className='logo'>
-        <Link to='/' onClick={handleLogoClick}>
-          <div className="logo">
-            <img src={ pokemonLogo } alt="Pokemon logo"/>
+      <div className={ isDarkModeOn ? 'logo-container dark-mode' : 'logo-container' } >
+        <Link to='/' onClick={ handleLogoClick }>
+          <div className={ isDarkModeOn ? 'logo dark-mode' : 'logo' } >
+            <img src={ pokemonLogo } alt='Pokemon logo' />
           </div>
         </Link>
       </div>
 
-      <div className='header-nav'>
+      <div className={ isDarkModeOn ? 'header-nav dark-mode' : 'header-nav' } >
         <nav>
-          <ul className='header-navbar'>
-            <li className='favorites-link'><Link to="/favorites">Favoritos</Link></li>
-            <li className='about-link'><Link to="/about">Sobre</Link></li>
+          <ul className={ isDarkModeOn ? 'header-navbar dark-mode' : 'header-navbar' } >
+            <li className={ isDarkModeOn ? 'favorites-link dark-mode' : 'favorites-link' } >
+              <Link to="/favorites">
+                Favoritos
+              </Link>
+            </li>
+  
+            <li className={ isDarkModeOn ? 'about-link dark-mode' : 'about-link' } >
+              <Link to="/about">
+                Sobre
+              </Link>
+            </li>
           </ul>
         </nav>
       
